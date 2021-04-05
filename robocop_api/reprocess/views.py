73f6c57f705d339a8b586eef0ea9e023ba0fdd4f
Alina Romantsova <alina@perlego.com>
2021-04-05 15:55:47 +0100

    work in progress

--- robocop_api/reprocess/views.py
+++ robocop_api/reprocess/views.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, current_app, request, make_response
-from werkzeug.datastructures import ImmutableMultiDict
-
 from robocop_api.utils.slack_utils import check_whitelist, echo_message, send_message
 
 reprocess_blueprint = Blueprint("reprocess", __name__)
@@ -9,13 +7,12 @@ reprocess_blueprint = Blueprint("reprocess", __name__)
 @reprocess_blueprint.route("/", methods=["POST"])
 def ping_request():
     try:
-        data: ImmutableMultiDict = request.form
-        data = data.to_dict()
+        data: dict = request.form.to_dict()
         response_url = data['response_url']
         check_whitelist(data, 'ping')
         echo_message(data)
         send_message('Pong', response_url)
         return make_response("", 200)
     except Exception as e:
-        current_app.logger.error(f"Failed: {e}")
+        current_app.logger.error(f"Ping command failed: {e}")
         raise
